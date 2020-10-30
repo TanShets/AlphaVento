9e9ff1756cb08a406fe323d72776640663b47ea5
TanShets <shettytanish02@gmail.com>
2020-10-30 19:11:47 +0530

    redirection

--- BetaArgenti/urls.py
+++ BetaArgenti/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.getHome, name = "homepage"),
+	path('', views.getHome, name = "default"),
 	path('/about', views.goAbout, name = "about")
 ]
