fa92723bf4334a51cf8b5dfed0e46874ca895e63
TanShets <shettytanish02@gmail.com>
2020-10-31 02:19:06 +0530

    Created Subjects and subject page

--- BetaArgenti/urls.py
+++ BetaArgenti/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.getHome, name = "default"),
-	path('/profile', views.goAbout, name = "profile")
+	path('/profile', views.goAbout, name = "profile"),
+	path('/subjects', views.viewSubjects, name = "subjects"),
+	path('/subject', views.viewSubject, name = "subject")
 ]
--- BetaArgenti/views.py
+++ BetaArgenti/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Subject
 
 def getHome(request):
 	words = ['Yeah yeah', 'nope', 'Yesterday']
@@ -17,3 +18,13 @@ def goAbout(request):
 		'title': "Profile"
 	}
 	return render(request, 'BetaArgenti/profile.html', context)
+
+def viewSubjects(request):
+	subjects = Subject.objects.all()
+	context = {'subjects': subjects, 'title': "View Subjects"}
+	return render(request, 'BetaArgenti/subjects.html', context)
+
+def viewSubject(request):
+	#subject = Subject.objects.get(id = subject_id)
+	context = {'title': "SubjectName"}
+	return render(request, 'BetaArgenti/subject.html', context)
